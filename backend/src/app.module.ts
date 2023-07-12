473a22f7c72d8218979cd03b86e8bd5b721428d5
catherine meng <mcatherine1994@gmail.com>
2023-07-12 16:26:11 -0700

    cleanup(16): - remove unused hello world code refs: #16

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import 'dotenv/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { IdimWebserviceModule } from './modules/idim-webservice/idim-webservice.module';
 
 @Module({
     imports: [ConfigModule.forRoot(), IdimWebserviceModule],
-    controllers: [AppController],
-    providers: [AppService],
 })
 export class AppModule {}
