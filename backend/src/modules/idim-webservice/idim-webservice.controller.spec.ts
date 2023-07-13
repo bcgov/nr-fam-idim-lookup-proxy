fb938bbe6d39a5a2a76c42ab6b9a8196fa460875
catherine meng <mcatherine1994@gmail.com>
2023-07-13 15:44:49 -0700

    test(16): - add test for controller method verifyIdirUser refs: #16

--- backend/src/modules/idim-webservice/idim-webservice.controller.spec.ts
+++ backend/src/modules/idim-webservice/idim-webservice.controller.spec.ts
@@ -1,20 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import 'dotenv/config';
 import { IdimWebserviceController } from './idim-webservice.controller';
 import { IdimWebserviceService } from './idim-webservice.service';
+import { IDIRUserResponse } from './idim-webservice.dto';
 
 describe('IdimWebserviceController', () => {
-  let controller: IdimWebserviceController;
+    let controller: IdimWebserviceController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [IdimWebserviceController],
-      providers: [IdimWebserviceService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [IdimWebserviceController],
+            providers: [IdimWebserviceService],
+        }).compile();
 
-    controller = module.get<IdimWebserviceController>(IdimWebserviceController);
-  });
+        controller = module.get<IdimWebserviceController>(
+            IdimWebserviceController
+        );
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+
+    describe('verifyIdirUser', () => {
+        const TEST_IDIR_USERID = 'CMENG';
+        const TEST_IDIR_USERID_NON_EXIST = 'test';
+        const TEST_REQUESTER_TYPE_CODE = 'Internal';
+
+        it('find non existing idir user', async () => {
+            const result = await controller.verifyIdirUser(
+                TEST_IDIR_USERID_NON_EXIST,
+                TEST_IDIR_USERID,
+                TEST_REQUESTER_TYPE_CODE
+            );
+            expect((result as IDIRUserResponse).found).toBe(false);
+            expect((result as IDIRUserResponse).userId).toBe(null);
+            expect((result as IDIRUserResponse).displayName).toBe(null);
+        });
+
+        it('find existing idir user', async () => {
+            const result = await controller.verifyIdirUser(
+                TEST_IDIR_USERID,
+                TEST_IDIR_USERID,
+                TEST_REQUESTER_TYPE_CODE
+            );
+            expect((result as IDIRUserResponse).found).toBe(true);
+            expect((result as IDIRUserResponse).userId).toBe(TEST_IDIR_USERID);
+        });
+
+        it('find using non existing requester id', async () => {
+            await expect(
+                controller.verifyIdirUser(
+                    TEST_IDIR_USERID,
+                    TEST_IDIR_USERID_NON_EXIST,
+                    TEST_REQUESTER_TYPE_CODE
+                )
+            ).rejects.toThrowError('Requester account cannot be found.');
+        });
+    });
 });
--- backend/src/modules/idim-webservice/idim-webservice.service.spec.ts
+++ backend/src/modules/idim-webservice/idim-webservice.service.spec.ts
@@ -1,18 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpException } from '@nestjs/common';
 import { IdimWebserviceService } from './idim-webservice.service';
+import { IDIRUserResponse } from './idim-webservice.dto';
 
 describe('IdimWebserviceService', () => {
-  let service: IdimWebserviceService;
+    let service: IdimWebserviceService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [IdimWebserviceService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [IdimWebserviceService],
+        }).compile();
 
-    service = module.get<IdimWebserviceService>(IdimWebserviceService);
-  });
+        service = module.get<IdimWebserviceService>(IdimWebserviceService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
