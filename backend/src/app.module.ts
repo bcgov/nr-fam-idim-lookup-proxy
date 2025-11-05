6ba15fbabef9e71d52b7366d2d10a887b6d50dcd
MCatherine <Mcatherine1994@gmail.com>
2025-11-05 13:24:54 -0800

    Debug IDIM Api password in test environment (#162)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import 'dotenv/config';
 import { IdimWebserviceModule } from './modules/idim-webservice/idim-webservice.module';
+import 'dotenv/config';
 
 @Module({
     imports: [ConfigModule.forRoot(), IdimWebserviceModule],
