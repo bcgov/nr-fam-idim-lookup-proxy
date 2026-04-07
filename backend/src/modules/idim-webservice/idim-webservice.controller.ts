9bf949b2756841186a05f19949c2823abc38aa20
Ian Liu <ianliuwk1019@gmail.com>
2026-04-07 12:59:01 -0700

    Rearrange and add annotation for deprecated endpoints.

--- backend/src/modules/idim-webservice/idim-webservice.controller.ts
+++ backend/src/modules/idim-webservice/idim-webservice.controller.ts
@@ -6,7 +6,7 @@ import {
     Query,
     UseGuards,
 } from '@nestjs/common';
-import { ApiQuery, ApiResponse, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { ApiQuery, ApiResponse, ApiSecurity, ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AuthGuard } from '../auth/auth.guard';
 import {
     BCEIDUserResponse,
@@ -25,24 +25,7 @@ export class IdimWebserviceController {
         private readonly idimWebserviceService: IdimWebserviceService
     ) {}
 
-    @Get('idir')
-    @ApiResponse({ status: HttpStatus.OK, type: IDIRUserResponse })
-    @ApiQuery({
-        name: 'requesterAccountTypeCode',
-        enum: RequesterAccountTypeCode,
-    })
-    async verifyIdirUser(
-        @Query('userId') userId: string,
-        @Query('requesterUserId') requesterUserId: string,
-        @Query('requesterAccountTypeCode')
-        requesterAccountTypeCode: RequesterAccountTypeCode
-    ): Promise<HttpException | IDIRUserResponse> {
-        return this.idimWebserviceService.verifyIdirUser(
-            userId,
-            requesterUserId,
-            requesterAccountTypeCode
-        );
-    }
+    // -- Below are for IDIR IDIM call
 
     @Get('idir-account-detail')
     @ApiResponse({ status: HttpStatus.OK, type: IDIRUserResponse })
@@ -57,25 +40,6 @@ export class IdimWebserviceController {
     }
 
     // -- Below are for BCeID IDIM call
-
-    @Get('bceid')
-    @ApiResponse({ status: HttpStatus.OK, type: BCEIDUserResponse })
-    @ApiQuery({
-        name: 'requesterAccountTypeCode',
-        enum: RequesterAccountTypeCode,
-    })
-    async verifyBceidUser(
-        @Query('userId') userId: string,
-        @Query('requesterUserGuid') requesterUserGuid: string,
-        @Query('requesterAccountTypeCode')
-        requesterAccountTypeCode: RequesterAccountTypeCode
-    ): Promise<HttpException | BCEIDUserResponse> {
-        return this.idimWebserviceService.verifyBceidUser(
-            userId,
-            requesterUserGuid,
-            requesterAccountTypeCode
-        );
-    }
 
     @Get('businessBceid')
     @ApiResponse({ status: HttpStatus.OK, type: BCEIDUserResponse })
@@ -94,6 +58,54 @@ export class IdimWebserviceController {
         return this.idimWebserviceService.verifyBusinessBceidUser(
             searchUserBy,
             searchValue,
+            requesterUserGuid,
+            requesterAccountTypeCode
+        );
+    }
+
+    // -- Below methods are deprecated endpoints, to be removed in a future release
+
+    @Get('idir')
+    /**
+     * @deprecated This endpoint is deprecated and will be removed in a future release.
+     */
+    @ApiResponse({ status: HttpStatus.OK, type: IDIRUserResponse })
+    @ApiQuery({
+        name: 'requesterAccountTypeCode',
+        enum: RequesterAccountTypeCode,
+    })
+    @ApiOperation({ deprecated: true })
+    async verifyIdirUser(
+        @Query('userId') userId: string,
+        @Query('requesterUserId') requesterUserId: string,
+        @Query('requesterAccountTypeCode')
+        requesterAccountTypeCode: RequesterAccountTypeCode
+    ): Promise<HttpException | IDIRUserResponse> {
+        return this.idimWebserviceService.verifyIdirUser(
+            userId,
+            requesterUserId,
+            requesterAccountTypeCode
+        );
+    }
+
+    @Get('bceid')
+    /**
+     * @deprecated This endpoint is deprecated and will be removed in a future release.
+     */
+    @ApiResponse({ status: HttpStatus.OK, type: BCEIDUserResponse })
+    @ApiQuery({
+        name: 'requesterAccountTypeCode',
+        enum: RequesterAccountTypeCode,
+    })
+    @ApiOperation({ deprecated: true })
+    async verifyBceidUser(
+        @Query('userId') userId: string,
+        @Query('requesterUserGuid') requesterUserGuid: string,
+        @Query('requesterAccountTypeCode')
+        requesterAccountTypeCode: RequesterAccountTypeCode
+    ): Promise<HttpException | BCEIDUserResponse> {
+        return this.idimWebserviceService.verifyBceidUser(
+            userId,
             requesterUserGuid,
             requesterAccountTypeCode
         );
